9569d41726dac7cb05d3fe485c6148e1e3fc7dfd
rnnewsworld <132123284+rnnewsworld@users.noreply.github.com>
2023-05-13 21:12:37 +0530

    Add files via upload

---
+++ ExamserverApplication.java
@@ -0,0 +1,38 @@
+package com.exam;
+
+import com.exam.model.Role;
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class ExamserverApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamserverApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception{
+		System.out.println("Server Fired");
+
+
+
+
+
+	}
+
+}
